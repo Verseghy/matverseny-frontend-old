4b0478c8e68bec66a229b0fea5c98a6e82041751
Szepesi Tibor <sztibi023@gmail.com>
2021-06-12 18:25:29 +0200

    feat: use aria-checked

--- src/components/switch.tsx
+++ src/components/switch.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import styles from '../styles/switch.module.scss'
-import classnames from 'classnames'
 
 export interface SwitchProps {
   value?: boolean,
@@ -9,7 +8,6 @@ export interface SwitchProps {
 
 const Switch: React.FC<SwitchProps> = ({ children, value, onClick }) => {
   const [internalValue, setIntervalValue] = useState(value ? value : false)
-  const classes = classnames(styles.switch, {[styles.on]: internalValue})
 
   const handleClick = () => {
     setIntervalValue((state: boolean) => !state)
@@ -21,12 +19,12 @@ const Switch: React.FC<SwitchProps> = ({ children, value, onClick }) => {
 
 
   if (!children) {
-    return <button role="switch" className={classes} onClick={handleClick} />
+    return <button role="switch" aria-checked={internalValue} className={styles.switch} onClick={handleClick} />
   }
 
   return (
     <label className={styles.label}>
-      <button role="switch" className={classes} onClick={handleClick} />
+      <button role="switch" aria-checked={internalValue} className={styles.switch} onClick={handleClick} />
       <span>{children}</span>
     </label>
   )
