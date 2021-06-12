f486115276359a68edbdc647d221f8c0abc82a26
Szepesi Tibor <sztibi023@gmail.com>
2021-06-12 18:28:07 +0200

    feat: forgot password

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -5,6 +5,8 @@ import Button from '../components/button'
 import styles from '../styles/forgot-password.module.scss'
 import { Formik, Form, Field, FieldProps, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
+import { AuthContext } from '../context/auth'
+import { ForgotPasswordRequest } from '../proto/auth_pb'
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required('Email kötelező'),
@@ -13,9 +15,13 @@ const validationSchema = Yup.object().shape({
 const initialValues = { email: '' }
 
 const ForgotPasswordPage: React.VFC = () => {
+  const { service } = React.useContext(AuthContext)
 
-  const onSubmit = (asd: any) => {
-    console.log('submit', asd)
+  const onSubmit = async (values: typeof initialValues) => {
+    let req = new ForgotPasswordRequest()
+      .setEmail(values.email)
+    
+    await service.forgotPassword(req, null)
   }
 
   return (
