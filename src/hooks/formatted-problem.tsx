9464683ad14e72e8b8899e03d6b5f28d7a79e928
Szepesi Tibor <sztibi023@gmail.com>
2021-06-20 03:24:45 +0200

    feat: custom error color in katex

--- src/hooks/formatted-problem.tsx
+++ src/hooks/formatted-problem.tsx
@@ -10,12 +10,20 @@ export const useFormatedProblem = (problemText: string) => {
     const block = problemText.match(/\$\$([^$]*)\$\$/g) || []
 
     for (const i of inline) {
-      const a = KaTeX.renderToString(i.substring(1, i.length - 1), { displayMode: false, throwOnError: false })
+      const a = KaTeX.renderToString(i.substring(1, i.length - 1), {
+        displayMode: false,
+        throwOnError: false,
+        errorColor: 'var(--red)',
+      })
       text = text.replace(i, a)
     }
 
     for (const b of block) {
-      const a = KaTeX.renderToString(b.substring(2, b.length - 2), { displayMode: true, throwOnError: false })
+      const a = KaTeX.renderToString(b.substring(2, b.length - 2), {
+        displayMode: true,
+        throwOnError: false,
+        errorColor: 'var(--red)',
+      })
       text = text.replace(b, a)
     }
 
