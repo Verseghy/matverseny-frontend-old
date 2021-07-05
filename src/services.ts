bcabf5c712c897537b5e534329b4a120b0428517
Szepesi Tibor <sztibi023@gmail.com>
2021-07-05 15:36:28 +0200

    fix: enableGRPCDevTools is not a function

--- src/services.ts
+++ src/services.ts
@@ -2,8 +2,9 @@ import { AdminClient } from './proto/AdminServiceClientPb'
 import { AuthClient } from './proto/AuthServiceClientPb'
 import { CompetitionClient } from './proto/CompetitionServiceClientPb'
 
+const devTools = (window as any).__GRPCWEB_DEVTOOLS__
 const enableGRPCDevTools =
-  process.env.NODE_ENV === 'development' ? (window as any).__GRPCWEB_DEVTOOLS__ : () => {}
+  process.env.NODE_ENV === 'development' && !!devTools ? devTools : () => {}
 
 export const authService = new AuthClient(process.env.REACT_APP_BACKEND_URL!, null, null)
 export const adminService = new AdminClient(process.env.REACT_APP_BACKEND_URL!, null, null)
