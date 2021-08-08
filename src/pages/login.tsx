8fd6c3d0210b158d92d12765d707e52b386a729d
Szepesi Tibor <sztibi023@gmail.com>
2021-08-09 01:05:28 +0200

    fix: Link import

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { Button, Input, Card, ErrorMessage as Message } from '../components'
 import styles from '../styles/login.module.scss'
 import { Formik, Form, Field, FieldProps, ErrorMessage } from 'formik'
