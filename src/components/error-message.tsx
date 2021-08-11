e33bbf083b3f01b51ff053b2923f07eb97ad7ea3
Szepesi Tibor <sztibi023@gmail.com>
2021-08-11 23:50:46 +0200

    feat: move error message translation to a hook

--- src/components/error-message.tsx
+++ src/components/error-message.tsx
@@ -1,39 +1,27 @@
-import React from 'react'
+import classnames from 'classnames'
+import React, { HTMLProps } from 'react'
+import { useTranslatedErrorMessage } from '../hooks/useTranslatedErrorMessage'
 import styles from '../styles/error-message.module.scss'
 
-export interface ErrorMessageProps {
+export interface ErrorMessageProps extends HTMLProps<HTMLDivElement> {
   message: string
+  translateMessage?: boolean
 }
 
-const errorMessages: { [key: string]: string } = {
-  E0000: 'not implemented',
-  E0001: 'Email megadása kötelező',
-  E0002: 'Jelszó megadása kötelező',
-  E0003: 'Hibás email cím vagy jelszó',
-  E0004: 'Adatbázis hiba',
-  E0005: 'Kriptográfiai hiba',
-  E0006: 'JWT hiba',
-  E0007: 'Név megadása kötelező',
-  E0008: 'Email formátuma hibás',
-  E0009: 'Iskola megadása kötelező',
-  E0010: 'A felhasználó már létezik',
-  E0011: 'Token lejárt',
-  E0012: 'Nincs jogosultság',
-  E0013: 'Hibás hely',
-  E0014: 'Nem található',
-  E0015: 'Hibás azonosító',
-  E0016: 'Nincs jogosultság',
-  E0017: 'Nincs csapat',
-  E0018: 'Sikertelen email küldés',
-  E0019: 'Hibás token',
-}
-
-export const ErrorMessage: React.VFC<ErrorMessageProps> = ({ message }) => {
+export const ErrorMessage: React.VFC<ErrorMessageProps> = ({
+  message,
+  translateMessage = true,
+  className,
+  ...props
+}) => {
+  const translatedMessage = useTranslatedErrorMessage(message)
   if (message === '') return null
 
-  let error
-  const match = message.match(/(E[0-9]{4}): .*/)
-  if (!match || !(error = errorMessages[match[1]])) error = 'Ismeretlen hiba'
+  const classes = classnames(styles.error, className)
 
-  return <div className={styles.error}>{error}</div>
+  return (
+    <div className={classes} {...props}>
+      {translateMessage ? translatedMessage : message}
+    </div>
+  )
 }
