393abf5b68db03ae5bf82a1eb3bc4739c479483d
Szepesi Tibor <sztibi023@gmail.com>
2021-11-12 23:35:56 +0100

    feat: team statistics diagram
    with hard coded data, because the backend does not provide enough
    information.

--- src/pages/superadmin.tsx
+++ src/pages/superadmin.tsx
@@ -12,6 +12,8 @@ import { createStreamService } from '../utils/streamService'
 import { getAtomValue, setAtomValue } from 'yauk'
 import { Map as ProtoMap } from 'google-protobuf'
 import { store } from '../state/store'
+import { Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+import classnames from 'classnames'
 
 const convertResultMap = (
   map: ProtoMap<string, GetResultsResponse.Result>
@@ -71,7 +73,7 @@ const TimeSettings: React.VFC = () => {
   }
 
   return (
-    <Card className={s.card}>
+    <Card className={classnames(s.card, s.timeCard)}>
       <h1>Idő Beállítások</h1>
       <Formik initialValues={times} onSubmit={onSubmit} validationSchema={timesValidation}>
         {({ isSubmitting }) => (
@@ -90,6 +92,75 @@ const TimeSettings: React.VFC = () => {
   )
 }
 
+const data = [
+  {
+    name: 'Valami Random csapatnév',
+    wrong: 99,
+    successful: 1,
+  },
+  {
+    name: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    wrong: 40,
+    successful: 10,
+  },
+  {
+    name: 'Some random',
+    wrong: 5,
+    successful: 100,
+  },
+  {
+    name: 'Még mindig kéne valami',
+    wrong: 70,
+    successful: 21,
+  },
+  {
+    name: 'Már nem kell sok',
+    wrong: 34,
+    successful: 42,
+  },
+  {
+    name: 'Utolsó előtti',
+    wrong: 10,
+    successful: 82,
+  },
+  {
+    name: 'Na végra már vége van',
+    wrong: 34,
+    successful: 0,
+  },
+  {
+    name: 'Na végra már vége van',
+    wrong: 12,
+    successful: 140,
+  },
+]
+
+interface TooltipProps {
+  label?: string
+}
+
+const ChartTooltip: React.VFC<TooltipProps> = ({ label }) => {
+  return <div className={s.tooltip}>{label}</div>
+}
+
+const TeamStatistics: React.VFC = () => {
+  return (
+    <Card className={classnames(s.card, s.teamsCard)}>
+      <h1>Csapat statisztika</h1>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart width={500} height={300} data={data}>
+          <Tooltip cursor={false} isAnimationActive={false} content={ChartTooltip} />
+          <CartesianGrid vertical={false} strokeDasharray="4" />
+          <XAxis tick={false} dataKey="name" tickCount={0} />
+          <YAxis domain={[0, 191]} width={30} />
+          <Bar dataKey="successful" stackId="a" isAnimationActive={false} fill="var(--primary)" />
+          <Bar dataKey="wrong" stackId="a" isAnimationActive={false} fill="var(--red)" />
+        </BarChart>
+      </ResponsiveContainer>
+    </Card>
+  )
+}
+
 const SuperAdminPage: React.VFC = () => {
   useEffect(() => {
     superadminService.start()
@@ -101,6 +172,7 @@ const SuperAdminPage: React.VFC = () => {
       <Suspense fallback="asd">
         <TimeSettings />
       </Suspense>
+      <TeamStatistics />
     </div>
   )
 }
